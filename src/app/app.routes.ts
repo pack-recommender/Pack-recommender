d36e9b5b793dd48464683ba4459cba5c0c2f5113
segalmc <segalmc@gmail.com>
2025-06-28 21:38:13 +0300

    add maintaince page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
+import { MaintenanceGuard } from './shared/guards/maintenance.guard';
 
 export const routes: Routes = [
-    { path: '', component: LandingComponent },
+    { path: 'maintenance', component: MaintenanceComponent },
+    { path: '', component: LandingComponent, canActivate: [MaintenanceGuard]
+     },
     {
-        path: '**',
-        component: NotFoundComponent
+        path: '**', component: NotFoundComponent, canActivate: [MaintenanceGuard]
     }
 ];
